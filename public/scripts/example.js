93c71541d9f0712c96d0a9700d73a564ebe4000e
Matthew Oppenheimer <mattopps@gmail.com>
2016-03-30 11:30:54 -0400

    strip out provided working code. Now Youy have a skeleton for a comments app

---
+++ public/scripts/example.js
@@ -0,0 +1,55 @@
+/**
+ * This file provided by Facebook is for non-commercial testing and evaluation
+ * purposes only. Facebook reserves all rights not expressly granted.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * FACEBOOK BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+var Comment = React.createClass({
+
+  },
+
+  render: function() {
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+
+      </div>
+    );
+  }
+});
+
+var CommentBox = React.createClass({
+
+  },
+  handleCommentSubmit: function(comment) {
+
+  },
+  getInitialState: function() {
+
+  },
+  componentDidMount: function() {
+
+  },
+  render: function() {
+    return (
+
+});
+
+var CommentList = React.createClass({
+
+});
+
+var CommentForm = React.createClass({
+
+});
+
+ReactDOM.render(
+  < />,
+
+);
